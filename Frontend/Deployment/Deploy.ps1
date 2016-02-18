5fcdee19e7509be3bf6162e334f482fa0dac905c
Sergei Isayeu <sergei@isayeu.com>
2016-02-18 17:13:14 -0600

    Updated Deploy file to deploy frontend to the S3

--- Frontend/Deployment/Deploy.ps1
+++ Frontend/Deployment/Deploy.ps1
@@ -1,36 +1,7 @@
-#
-# Deploy.ps1
-#
-# deploying this to the S3 bucket on the cloud
-# setting permissions pn the bucket level after update
-# 
 
-$bucketName = 'trenches-dev.isayeu.com'
-
-
-function UploadFileToS3([String] $filePath)
-{
-    Write-Host $filePath;
-
-
-}
-
-$filesToUpload = ('..\admin\index.html', '..\content\*.*', '..\data.json','..\favicon.ico', '..\index.html');
-ForEach($mask in $filesToUpload)
-{
-	Write-Host $mask
-    $filesByMask = Get-ChildItem $mask
-    ForEach($file in $filesByMask)
-    {
-        
-        UploadFileToS3($file);
-    }
-    Write-Host $item
-}
-
-$bucket = Get-S3Bucket -BucketName $bucketName -ProfileName DefaultProfile
-Write-Host $bucket.BucketName
-Write-Host $bucket.CreationDate
-
-
+$folder = (Get-Item -Path "..\" -Verbose).FullName
+$bucketName = 'your-bucket-name-here'
+Write-Host 'Current invocation path:', $folder
+#Write-S3Object -BucketName 'staging.positivitymeter.com' -Folder (Get-Item -Path "..\" -Verbose).FullName -CannedACLName 'public-read' -ProfileName 'DefaultProfile' -KeyPrefix / -Recurse
+Write-S3Object -BucketName $bucketName -Folder (Get-Item -Path "..\" -Verbose).FullName -CannedACLName 'public-read' -ProfileName 'DefaultProfile' -KeyPrefix / -Recurse
 
